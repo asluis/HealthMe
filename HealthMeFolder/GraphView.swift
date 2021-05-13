359db9754790b0db5495127ec1bc47f8251fa869
asluis <asluisinfo@gmail.com>
2021-05-12 17:41:12 -0700

    Starting work on Grapghview

---
+++ HealthMeFolder/GraphView.swift
@@ -0,0 +1,20 @@
+//
+//  GraphView.swift
+//  HealthMe
+//
+//  Created by Luis Alvarez on 5/12/21.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+    }
+}
--- HealthMeFolder/HealthMeApp.swift
+++ HealthMeFolder/HealthMeApp.swift
@@ -1,7 +1,4 @@
 //
-//  CMPE_131_UIApp.swift
-//  CMPE_131_UI
-//
 //  Created by Luis Alvarez on 3/22/21.
 //
 
