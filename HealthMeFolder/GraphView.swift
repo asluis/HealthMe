1a6866b8a5b5dd91ef15fed6cc3b1f6fd54d0fbe
asluis <asluisinfo@gmail.com>
2021-06-20 20:23:07 -0700

    Slightly modified graph view

--- HealthMeFolder/GraphView.swift
+++ HealthMeFolder/GraphView.swift
@@ -11,7 +11,7 @@ struct GraphView: View {
     let user:User
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, \(user.name)")
     }
 }
 
