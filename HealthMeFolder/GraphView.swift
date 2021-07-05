5c94398f67f18b5b752a4f363929b12f41a5fd4c
asluis <asluisinfo@gmail.com>
2021-07-05 13:09:19 -0700

    Bar now has rounded corners in right side only and now has a border around it. Refactored code to make creating Bars cleaner.

--- HealthMeFolder/GraphView.swift
+++ HealthMeFolder/GraphView.swift
@@ -11,31 +11,59 @@ struct GraphView: View {
     let user:User
     
     var body: some View {
-        GeometryReader{ geo in
-            Bar(scalar: 0.5)
-                .fill(Color.red)
-                .frame(width: geo.size.width, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        }
+        Bar(scalar: 0.5, radius: 25)
         
     }
 }
 
-// Horizontal Bar
-struct Bar: Shape{
-    let scalar:CGFloat // Scales size of rect given relationship with data
+
+
+
+
+
+/**
+ Creates a horizontal bar with its base being on the leftmost side of the view it's found in. The right side of the bar has rounded edges.
+ */
+struct Bar: View{
     
-    // Starts drawing in top right of given rect/frame
-    func path(in rect: CGRect) -> Path {
-        let adjustedWidthX = scalar * rect.maxX
-        
-        var path = Path()
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: adjustedWidthX, y: rect.minY))// needs to be round
-        path.addLine(to: CGPoint(x: adjustedWidthX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY)) // Needs to be round
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        return path
+    let scalar: CGFloat
+    let radius: CGFloat
+    
+    var body: some View{
+        GeometryReader{ geo in
+            MyBar(myScalar: 0.5, myRadius: 25)
+                .strokeBorder(Color.black, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+                .background(MyBar(myScalar: 0.5, myRadius: 25).fill(Color.blue))
+                .frame(width: geo.size.width, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+        }
     }
+    
+    struct MyBar: InsettableShape{
+        var insetAmount:CGFloat = 0
+        let myScalar:CGFloat
+        let myRadius:CGFloat
+        
+        func inset(by amount: CGFloat) -> some InsettableShape {
+            var shape = self
+            shape.insetAmount += amount
+            return shape
+        }
+        
+        func path(in rect: CGRect) -> Path {
+            let adjustedWidthX = myScalar * rect.maxX
+            
+            var path = Path()
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: adjustedWidthX - myRadius, y: rect.minY))
+            path.addArc(center: CGPoint(x: adjustedWidthX - myRadius, y: rect.minY + myRadius), radius: myRadius - insetAmount, startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+            path.addLine(to: CGPoint(x: adjustedWidthX, y: rect.maxY - myRadius))
+            path.addArc(center: CGPoint(x: adjustedWidthX - myRadius, y: rect.maxY - myRadius), radius: myRadius - insetAmount, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            return path
+        }
+    }
+
 }
 
 struct GraphView_Previews: PreviewProvider {
